20ec56e92d711d426e2f95f368922f3e175a83fe
javier <148455736+fjzamora93@users.noreply.github.com>
2024-06-22 11:32:55 +0200

    update

--- models/cart.js
+++ models/cart.js
@@ -15,23 +15,27 @@ module.exports = class Cart {
       if (!err) {
         cart = JSON.parse(fileContent);
       }
-      // Analyze the cart => Find existing product
+      // Buscamos por el índice. El índice lo necesitamos para saber en qué parte del array está el producto que vamos a actualizar.
       const existingProductIndex = cart.products.findIndex(
         prod => prod.id === id
       );
+
+      //Pero no solo hay que saber su posición... también necesitamos el producto en sí mismo, para actualizarlo.
       const existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
-      // Add new product/ increase quantity
+
+      // Incrementar cantidad si existe el producto.
       if (existingProduct) {
-        updatedProduct = { ...existingProduct };
-        updatedProduct.qty = updatedProduct.qty + 1;
-        cart.products = [...cart.products];
-        cart.products[existingProductIndex] = updatedProduct;
+        updatedProduct = { ...existingProduct }; //los tres puntos es para hacer una copia del objeto
+        updatedProduct.qty = updatedProduct.qty + 1; //modificamos la cantidad sumándole 1
+        cart.products = [...cart.products]; //! ??? me podré cargar esta línea
+        cart.products[existingProductIndex] = updatedProduct; // usamos el índice y lo remplazamos por el producto modificado
       } else {
+        //En el else creamos una copia del carro y le añadimos el producto actualizado, sin más. 
         updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
       }
-      cart.totalPrice = cart.totalPrice + +productPrice;
+      cart.totalPrice = cart.totalPrice + +productPrice; //el +productPrice convierte a número el valor de esa varaiable.
       fs.writeFile(p, JSON.stringify(cart), err => {
         console.log(err);
       });
